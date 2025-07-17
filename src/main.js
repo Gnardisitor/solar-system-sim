82b0a12f4775d626b85b67a3d2ddd38acc6feb38
Gnardisitor <bajanica.dragos@gmail.com>
2025-07-17 13:38:27 -0400

    Batched API calls, simplified changing step size and switching between methods

--- src/main.js
+++ src/main.js
@@ -2,31 +2,35 @@ import * as THREE from "three"
 import {OrbitControls} from "three/examples/jsm/controls/OrbitControls.js"
 
 import createModule from "./nbody.js"
-let init_body;
-let verlet;
-let get_x;
-let get_y;
-let get_z;
+let initBody;
+let simulateStep;
+let getX;
+let getY;
+let getZ;
 
 const scene = new THREE.Scene();
 const camera = new THREE.PerspectiveCamera(75, window.innerWidth / window.innerHeight, 0.1, 1000);
 
+const masses = [1.989E30, 3.301E23, 4.868E24, 5.972E24, 6.417E23, 1.898E27, 5.683E26, 8.681E25, 1.024E26]
+
 const clock = new THREE.Clock();
-const update = 0.05;
+const update = 0.025;
 let max = 10000;
-let meshes;
-let step = 0;
+let meshes = [];
+const step = 0.5;
+let currentStep = 0;
 let time = 0;
+const method = 1;
 
 const ambientLight = new THREE.AmbientLight(0x404040, 10);
 scene.add(ambientLight);
 
 await createModule().then((Module) => {
-    init_body = Module.cwrap('init_body', null, ['number', 'number', 'number', 'number', 'number', 'number', 'number']);
-    verlet = Module.cwrap('verlet', null, ['number']);
-    get_x = Module.cwrap('get_x', 'number', ['number']);
-    get_y = Module.cwrap('get_y', 'number', ['number']);
-    get_z = Module.cwrap('get_z', 'number', ['number']);
+    initBody = Module.cwrap('init_body', null, ['number', 'number', 'number', 'number', 'number', 'number', 'number', 'number']);
+    simulateStep = Module.cwrap('simulate_step', null, ['number', 'number']);
+    getX = Module.cwrap('get_x', 'number', ['number']);
+    getY = Module.cwrap('get_y', 'number', ['number']);
+    getZ = Module.cwrap('get_z', 'number', ['number']);
 });
 await initBodies(2000);
 
@@ -40,18 +44,21 @@ camera.position.z = 5;
 
 async function parseData(id, year) {
     const proxy = "https://proxy.corsfix.com/?";
+    //const proxy =  "http://localhost:3000/jpl?url=";
+    const url = `https://ssd.jpl.nasa.gov/api/horizons.api?format=text&COMMAND='${id}'&CENTER='@0'&CSV_FORMAT='YES'&EPHEM_TYPE='VECTOR'&VEC_TABLE='2'&OUT_UNITS='AU-D'&START_TIME='${year}-01-01'&STOP_TIME='${year}-01-02'&STEP_SIZE='2%20d'`
     const start = "$$SOE";
     const end = "$$EOE";
 
-    const response = await fetch(`${proxy}https://ssd.jpl.nasa.gov/api/horizons.api?format=text&COMMAND='${id}'&CENTER='@0'&CSV_FORMAT='YES'&EPHEM_TYPE='VECTOR'&VEC_TABLE='2'&OUT_UNITS='AU-D'&START_TIME='${year}-01-01'&STOP_TIME='${year}-01-02'&STEP_SIZE='2%20d'`);
+    //const response = await fetch(proxy + encodeURIComponent(url));
+    const response = await fetch(proxy + url);
     const data = await response.text();
 
-    let vectors = data.split(start)[1];
-    vectors = vectors.split(end)[0];
-    vectors = vectors.split(",");
+    let vecData = data.split(start)[1];
+    vecData = vecData.split(end)[0];
+    vecData = vecData.split(",");
 
-    let pos = vectors.slice(2, 5).map(e => scientificNotation(e));
-    let vel = vectors.slice(5, 8).map(e => scientificNotation(e));
+    let pos = vecData.slice(2, 5).map(e => scientificNotation(e));
+    let vel = vecData.slice(5, 8).map(e => scientificNotation(e));
     return [pos, vel];
 }
 
@@ -60,35 +67,41 @@ function scientificNotation(num) {
     return Number(nums[0]) * 10 ** Number(nums[1]);
 }
 
+async function batchFetch(ids, year, batchSize) {
+    const results = [];
+    for (let i = 0; i < ids.length; i += batchSize) {
+        const batch = ids.slice(i, i + batchSize);
+        const batchResults = await Promise.all(batch.map(id => parseData(id, year)));
+        results.push(...batchResults);
+    }
+    return results;
+}
+
 async function initBodies(year) {
     const ids = [10, 199, 299, 399, 499, 599, 699, 799, 899];
     const colors = [0xffff00, 0x666699, 0x993333, 0x0099ff, 0xcc3300, 0x996600, 0xffcc99, 0x99ccff, 0x6666ff]
-
     const geometry = new THREE.SphereGeometry(0.2, 10, 10);
 
-    meshes = []
-    console.log(meshes);
-
+    const vectors = await batchFetch(ids, year, 2);
     for (let i = 0; i < ids.length; i++) {
         meshes.push(new THREE.Mesh(geometry, new THREE.MeshStandardMaterial({color: colors[i]})));
-        const vec = await parseData(ids[i], year);
+        const vec = vectors[i];
         console.log(vec);
         scene.add(meshes[i]);
         meshes[i].position.set(...vec[0]);
 
         // Initialize the body in the C module
-        init_body(i, vec[0][0], vec[0][1], vec[0][2], vec[1][0], vec[1][1], vec[1][2]);
+        initBody(i, masses[i], vec[0][0], vec[0][1], vec[0][2], vec[1][0], vec[1][1], vec[1][2]);
     }
 }
 
-function simulate(step) {
-    verlet(step);
-    console.log(`Moved to step ${step}!`);
+function simulate() {
+    simulateStep(method, step);
+    console.log(`Moved to step ${currentStep}!`);
     for (let i = 0; i < meshes.length; i++) {
-        let x, y, z;
-        x = get_x(i);
-        y = get_y(i);
-        z = get_z(i);
+        const x = getX(i);
+        const y = getY(i);
+        const z = getZ(i);
         console.log(`Body ${i} new position: (${x}, ${y}, ${z})`);
         meshes[i].position.set(x, y, z);
     }
@@ -96,11 +109,12 @@ function simulate(step) {
 
 function animate() {
     time += clock.getDelta();
-    if (time >= update && step < max) {
-        simulate(++step);
+    if (time >= update && currentStep < max) {
+        currentStep++;
+        simulate();
         time = 0;
     }
-    else if (step >= max) {
+    else if (currentStep >= max) {
         console.log("Reached max number of steps!");
     }
 
--- src/nbody.c
+++ src/nbody.c
@@ -1,6 +1,5 @@
-//#include <emscripten.h>	// EMSCRIPTEN
-#include <stdlib.h>
-#include <string.h>
+//#include <stdlib.h>
+//#include <string.h>
 #include <stdio.h>
 #include <math.h>
 
@@ -10,18 +9,16 @@
 #define TIME	86400.0					// Conversion factor from s to d
 #define ACC		(TIME * TIME) / DIST	// Conversion factor from m/s^2 to AU/d^2
 
-// Constant bodiess for bodies
-const double masses[9] = {1.989E30, 3.301E23, 4.868E24, 5.972E24, 6.417E23, 1.898E27, 5.683E26, 8.681E25, 1.024E26};	// Masses in kg
-
-// Functions
-void set_acc();
+// Constants for PEFRL
+#define XI		+0.1786178958448091E+00			// ξ
+#define LAMBDA	-0.2123418310626054E+00			// λ
+#define CHI		-0.6626458266981849E-01			// χ
+#define P1		(1.0 - (2.0 * LAMBDA)) * 0.5	// (1 - 2λ) / 2
+#define P2		1.0 - (2.0 * (CHI + XI))		// 1 - 2(χ + ξ)
 
 // Body struct and bodies
 typedef struct {
 	double mass;		// Mass
-	//double **hist;		// Previous positions
-
-	// Current vectors
 	double x, y, z;		// Position
 	double vx, vy, vz;	// Velocity
 	double ax, ay, az;	// Acceleration
@@ -29,44 +26,46 @@ typedef struct {
 Body bodies[9];			// Array of all main 9 bodies in Solar System
 
 // Required simulation variables
-long unsigned int total;	// Total number of steps to simulate
-long unsigned int time;		// Amount of time to simulate (d)
-double step = 1;			// Time per step to simulate (d)
-int year;					// Initial year of simulation
-int size = 9;				// Number of bodies
+//long unsigned int total;	// Total number of steps to simulate
+//long unsigned int time;	// Amount of time to simulate (d)
+#define SIZE 9				// Number of bodies (Temporarily set to 9)
 
-void init_body(int body, double x, double y, double z, double vx, double vy, double vz) {
-	bodies[body].mass = masses[body];
+// Function prototypes
+void euler(double step);
+void verlet(double step);
+void pefrl(double step);
+
+/* Functions used in JavaScript code */
+
+void init_body(int body, double mass, double x, double y, double z, double vx, double vy, double vz) {
+	// Set mass
+	bodies[body].mass = mass;
+
+	// Set position vector
 	bodies[body].x = x;
 	bodies[body].y = y;
 	bodies[body].z = z;
+
+	// Set velocity vector
 	bodies[body].vx = vx;
 	bodies[body].vy = vy;
 	bodies[body].vz = vz;
 }
 
-void verlet(unsigned int t) {
-	// Compute position at half-step
-	for (unsigned int i = 0; i < size; i++) {
-		bodies[i].x = bodies[i].x + (0.5 * step * bodies[i].vx);
-		bodies[i].y = bodies[i].y + (0.5 * step * bodies[i].vy);
-		bodies[i].z = bodies[i].z + (0.5 * step * bodies[i].vz);
-	}
-
-	// Compute acceleration for all bodies
-	set_acc();
-
-	// Compute next half-step
-	for (unsigned int i = 0; i < size; i++) {
-		// Compute new velocity
-		bodies[i].vx = bodies[i].vx + (step * bodies[i].ax);
-		bodies[i].vy = bodies[i].vy + (step * bodies[i].ay);
-		bodies[i].vz = bodies[i].vz + (step * bodies[i].az);
-
-		// Compute new position at the end of the step
-		bodies[i].x = bodies[i].x + (0.5 * step * bodies[i].vx);
-		bodies[i].y = bodies[i].y + (0.5 * step * bodies[i].vy);
-		bodies[i].z = bodies[i].z + (0.5 * step * bodies[i].vz);
+void simulate_step(int method, double step) {
+	switch (method) {
+		case 0: // Euler
+			euler(step);
+			break;
+		case 1: // Verlet
+			verlet(step);
+			break;
+		case 2: // PEFRL
+			pefrl(step);
+			break;
+		default:
+			printf("Unknown method %d!\n", method);
+			break;
 	}
 }
 
@@ -82,15 +81,17 @@ double get_z(int body) {
 	return bodies[body].z;
 }
 
+/* Helper functions */
+
 void set_acc(void) {
-	for (unsigned int i = 0; i < size; i++) {
+	for (int i = 0; i < SIZE; i++) {
 		bodies[i].ax = 0.0;
 		bodies[i].ay = 0.0;
 		bodies[i].az = 0.0;
 	}
 
-	for (unsigned int i = 0; i < size - 1; i++) {
-		for (long unsigned int j = i + 1; j < size; j++) {
+	for (int i = 0; i < SIZE - 1; i++) {
+		for (int j = i + 1; j < SIZE; j++) {
 			// Compute vector
 			double dx, dy, dz;
 			dx = DIST * (bodies[j].x - bodies[i].x);
@@ -98,7 +99,7 @@ void set_acc(void) {
 			dz = DIST * (bodies[j].z - bodies[i].z);
 
 			// Square root of vector
-			double r= sqrt((dx * dx) + (dy * dy) + (dz * dz));
+			double r = sqrt((dx * dx) + (dy * dy) + (dz * dz));
 
 			// Compute acceleration and assign to each body
 			double mag = (ACC * G) / (r * r * r);
@@ -115,5 +116,108 @@ void set_acc(void) {
 			bodies[j].ay += mag * m2 * dy;
 			bodies[j].az += mag * m2 * dz;
 		}
+	}
+}
+
+void euler(double step) {
+	// Compute acceleration for all bodies
+	set_acc();
+
+	// Compute new velocity and position
+	for (int i = 0; i < SIZE; i++) {
+		// Compute new velocity
+		bodies[i].vx = bodies[i].vx + (step * bodies[i].ax);
+		bodies[i].vy = bodies[i].vy + (step * bodies[i].ay);
+		bodies[i].vz = bodies[i].vz + (step * bodies[i].az);
+
+		// Compute new position
+		bodies[i].x = bodies[i].x + (step * bodies[i].vx);
+		bodies[i].y = bodies[i].y + (step * bodies[i].vy);
+		bodies[i].z = bodies[i].z + (step * bodies[i].vz);
+	}
+}
+
+void verlet(double step) {
+	// Compute position at half-step
+	for (int i = 0; i < SIZE; i++) {
+		bodies[i].x = bodies[i].x + (0.5 * step * bodies[i].vx);
+		bodies[i].y = bodies[i].y + (0.5 * step * bodies[i].vy);
+		bodies[i].z = bodies[i].z + (0.5 * step * bodies[i].vz);
+	}
+
+	// Compute acceleration for all bodies
+	set_acc();
+
+	// Compute next half-step
+	for (int i = 0; i < SIZE; i++) {
+		// Compute new velocity
+		bodies[i].vx = bodies[i].vx + (step * bodies[i].ax);
+		bodies[i].vy = bodies[i].vy + (step * bodies[i].ay);
+		bodies[i].vz = bodies[i].vz + (step * bodies[i].az);
+
+		// Compute new position at the end of the step
+		bodies[i].x = bodies[i].x + (0.5 * step * bodies[i].vx);
+		bodies[i].y = bodies[i].y + (0.5 * step * bodies[i].vy);
+		bodies[i].z = bodies[i].z + (0.5 * step * bodies[i].vz);
+	}
+}
+
+void pefrl(double step) {
+	for (int i = 0; i < SIZE; i++) {
+		bodies[i].x = bodies[i].x + (XI * step * bodies[i].vx);
+		bodies[i].y = bodies[i].y + (XI * step * bodies[i].vy);
+		bodies[i].z = bodies[i].z + (XI * step * bodies[i].vz);
+	}
+
+	set_acc();
+	for (int i = 0; i < SIZE; i++) {
+		bodies[i].vx = bodies[i].vx + (P1 * step * bodies[i].ax);
+		bodies[i].vy = bodies[i].vy + (P1 * step * bodies[i].ay);
+		bodies[i].vz = bodies[i].vz + (P1 * step * bodies[i].az);
+	}
+
+	for (int i = 0; i < SIZE; i++) {
+		bodies[i].x = bodies[i].x + (CHI * step * bodies[i].vx);
+		bodies[i].y = bodies[i].y + (CHI * step * bodies[i].vy);
+		bodies[i].z = bodies[i].z + (CHI * step * bodies[i].vz);
+	}
+
+	set_acc();
+	for (int i = 0; i < SIZE; i++) {
+		bodies[i].vx = bodies[i].vx + (LAMBDA * step * bodies[i].ax);
+		bodies[i].vy = bodies[i].vy + (LAMBDA * step * bodies[i].ay);
+		bodies[i].vz = bodies[i].vz + (LAMBDA * step * bodies[i].az);
+	}
+
+	for (int i = 0; i < SIZE; i++) {
+		bodies[i].x = bodies[i].x + (P2 * step * bodies[i].vx);
+		bodies[i].y = bodies[i].y + (P2 * step * bodies[i].vy);
+		bodies[i].z = bodies[i].z + (P2 * step * bodies[i].vz);
+	}
+
+	set_acc();
+	for (int i = 0; i < SIZE; i++) {
+		bodies[i].vx = bodies[i].vx + (LAMBDA * step * bodies[i].ax);
+		bodies[i].vy = bodies[i].vy + (LAMBDA * step * bodies[i].ay);
+		bodies[i].vz = bodies[i].vz + (LAMBDA * step * bodies[i].az);
+	}
+
+	for (unsigned int i = 0; i < SIZE; i++) {
+		bodies[i].x = bodies[i].x + (CHI * step * bodies[i].vx);
+		bodies[i].y = bodies[i].y + (CHI * step * bodies[i].vy);
+		bodies[i].z = bodies[i].z + (CHI * step * bodies[i].vz);
+	}
+
+	set_acc();
+	for (int i = 0; i < SIZE; i++) {
+		bodies[i].vx = bodies[i].vx + (P1 * step * bodies[i].ax);
+		bodies[i].vy = bodies[i].vy + (P1 * step * bodies[i].ay);
+		bodies[i].vz = bodies[i].vz + (P1 * step * bodies[i].az);
+	}
+
+	for (int i = 0; i < SIZE; i++) {
+		bodies[i].x = bodies[i].x + (XI * step * bodies[i].vx);
+		bodies[i].y = bodies[i].y + (XI * step * bodies[i].vy);
+		bodies[i].z = bodies[i].z + (XI * step * bodies[i].vz);
 	}
 }
