d4c4b17e7a8979d7bfc8a27f108dbf2e72197fa0
Gnardisitor <bajanica.dragos@gmail.com>
2025-08-01 14:37:31 -0400

    Fix for dual HTML files not loading

--- vite.config.js
+++ vite.config.js
@@ -1,4 +1,14 @@
+import { defineConfig } from 'vite';
+
 // vite.config.js
-export default {
+export default defineConfig({
+    build: {
+        rollupOptions: {
+            input: {
+                main: 'index.html',
+                interactive: 'interactive.html'
+            }
+        }
+    },
     base: '/solar-system-sim/'  // For GitHub Pages deployment
-}
+});
